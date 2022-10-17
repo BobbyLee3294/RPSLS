3a6e4ccb7d98521b042bfdf0b16419e67623767c
Bobby Lee <thayungbobby@gmail.com>
2022-10-17 19:25:10 -0400

    added inital functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # as a developer, I want to find a way to properly incorporate inheritance into my game
-
 # as a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary
 # as a developer, I want to store all of the gesture options/choices in a list. I want to find a way to utilize the list of gestures within my code (display gesture options, assign player a gesture, etc.)
-# as a player, I want the correct palyer to win a given round based on the choices made by each player
+# as a player, I want the correct player to win a given round based on the choices made by each player
 # as a player, I want the gmae of RPSLS to be at minimum a "best of three" to decide a winner
 # as a player, I want the option of a single player (human vs AI) or a multiplayer (human vs human) game
