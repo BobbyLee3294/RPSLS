78a9df6b0f177aa181a9b57f240d3d9b31588335
Bobby Lee <thayungbobby@gmail.com>
2022-10-18 21:42:22 -0400

    deleted gestures files

--- gestures/lizard.py
+++
@@ -1,7 +0,0 @@
- # as a developer, I want to create a class for the lizard gesture
-class Lizard:
-    def __init__(self):
-        self.name = "lizard"
-        self.beats = ["paper", "spock"]
-        self.beaten_by = ["rock", "scissors"]
- # as a developer, I want to create a method for the lizard gesture that will compare itself to the other gestures
--- gestures/paper.py
+++
@@ -1,7 +0,0 @@
-# as a developer, I want to create a class for the paper gesture
-class Paper:
-    def __init__(self):
-        self.name = "paper"
-        self.beats = ["rock", "spock"]
-        self.beaten_by = ["scissors", "lizard"]
-# as a developer, I want to create a method for the paper gesture that will compare itself to the other gestures
