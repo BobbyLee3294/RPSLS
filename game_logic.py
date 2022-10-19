c7a81ccac5f232b611ffde42a5d31507c895abb8
Bobby Lee <thayungbobby@gmail.com>
2022-10-18 21:41:12 -0400

    added new file

---
+++ game_logic.py
@@ -0,0 +1,23 @@
+# this file will be used to create the game logic for our RPSLS game
+# we will need to import the classes from the player and gestures directories
+from player.player import Player
+from player.human import Human
+from player.computer import Computer
+
+# we will create a list that will hold the gestures
+gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+# we will need to create a method that will allow us to display a greeting to the user
+def display_greeting():
+    pass
+# we will need to create a method that will allow us to display the rules of the game
+def display_rules():
+    pass
+# we will neeed to create a method that will allow us to determine which game mode the user wants to play
+def determine_game_mode():
+    pass
+# we will need to create a method that will allow us to run the game
+def run_game():
+    pass
+# we will need to create a method that will allow us to display the winner of the game
+def display_winner():
+    pass
